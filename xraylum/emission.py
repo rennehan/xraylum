745dcbe2eb50e7a17e1fbd4a346c1fd1c6cf4b7a
Douglas Rennehan <rennehan@uvic.ca>
2022-07-11 06:59:56 -0400

    Pyatomdb completely changed how we can compute an Xray spectrum. I updated these tools to include the new method. The module now returns a grid of emitted power in AtomicNumber and Temperature as an interp1d object.

--- xraylum/emission.py
+++ xraylum/emission.py
@@ -1,127 +1,95 @@
-import astropy.io.fits as fits
+import pyatomdb
 import numpy as np
-import h5py
-from scipy.interpolate import interp1d
+from scipy import interpolate
 
-def calculate(emissivity_files, gas_data):
-    # gas_data is a dictionary with:
-    # gas_data['volume']
-    # gas_data['densities']
-    # gas_data['temperatures']
-    # gas_data['carbon']
-    # gas_data['oxygen']
-    # gas_data['silicon']
-    # gas_data['iron']
-    # gas_data['ne']
-    # gas_data['nh']
-
-    bands = np.loadtxt(bands_file)
-
-    line_file = 'atomdb/apec_line.fits'
-    coco_file = 'atomdb/apec_coco.fits'
-
-    line = fits.open(line_file)
-    continuum = fits.open(coco_file)
-
-    # H, He, C, O, Si, Fe
-    # These have to be ordered by atomic number!
-    # 26 will hold everything else including iron
-    atomic = [1, 2, 6, 8, 14, 26]
-
-    min_temperature = 5.0e5
-
-    # Solar values that Liang et al. (2016) employ
-    solar = {'hydrogen': 0.7381,
-             'helium': 0.2485,
-             'carbon': 0.00307,
-            'oxygen': 0.009618,
-            'silicon': 0.0007109,
-            'iron': 0.001267}
-
-
-
-    temps = continuum[1].data['kT'] * 11604.505 * 1.0e3  # Result in K
-
-    num_atomic = len(atomic)
-    num_temps = len(temps)
-    log_temps = np.log10(temps)
-
-    def calculate_lambda(emissivities_file):
-        eps_total = np.zeros((num_atomic, len(gas_data['temperature'])))
-        eps_total_wide_band = np.copy(eps_total)
-
-        with h5py.File(emissivities_file, 'r') as f:
-            cont_emissivities = np.array(f['/continuum_emission'])
-            line_emissivities = np.array(f['/line_emission'])
+def _calculate_power(cie, elements, temperatures):
+    res = {}
+    res['power'] = {}
+    res['temperature'] = []
     
-        log_cont = np.log10(cont_emissivities)
-        log_line = np.log10(line_emissivities)
+    # Zero out all abundances
+    cie.set_abund(np.arange(1, 31), 0.0)
+    kT_list = temperatures * pyatomdb.const.KBOLTZ
+    average_energies = (cie.ebins_out[1:] + cie.ebins_out[:-1]) / 2.0
 
-        num_interp_points = num_temps * 10
-        new_temps = np.linspace(np.amin(log_temps), np.amax(log_temps), num_interp_points)
+    start_idx = 0
+    if 0 in elements:
+        start_idx = 1
+    
+    for i, kT in enumerate(kT_list):
+        T = temperatures[i]
 
-        new_cont = np.zeros((num_atomic, num_interp_points))
-        new_line = np.copy(new_cont)
+        res['temperature'].append(T)
+        res['power'][i] = {}
 
-        # Loop over the atomic number axis
-        for j in range(num_atomic):
-            # do continuum emission
-            non_zero_idx = np.where(cont_emissivities[j] != 0)
-            if len(non_zero_idx[0]) >= 2:
-                f = interp1d(log_temps[non_zero_idx], log_cont[j][non_zero_idx])
-
-                new_temps_idx = np.where((new_temps >= np.amin(log_temps[non_zero_idx])) &
-                                        (new_temps <= np.amax(log_temps[non_zero_idx])))
-                new_cont[j][new_temps_idx] = f(new_temps[new_temps_idx])
-
-            # now do line emission
-            non_zero_idx = np.where(line_emissivities[j] != 0)
-            if len(non_zero_idx[0]) >= 2:
-                f = interp1d(log_temps[non_zero_idx], log_line[j][non_zero_idx])
-
-                new_temps_idx = np.where((new_temps >= np.amin(log_temps[non_zero_idx])) &
-                                        (new_temps <= np.amax(log_temps[non_zero_idx])))
-                new_line[j][new_temps_idx] = f(new_temps[new_temps_idx])
-
-        new_cont[np.where(new_cont == 0)] = np.nan
-        new_line[np.where(new_line == 0)] = np.nan
-
-        new_temps = 10**new_temps
-        new_cont = 10**new_cont
-        new_line = 10**new_line
-
-        new_cont[np.isnan(new_cont)] = 0
-        new_line[np.isnan(new_line)] = 0
-
-        for i in range(len(new_temps)):
-            if new_temps[i] < min_temperature:
-                new_temps[i] = min_temperature
-
-            print('Do temperature: %g K' % new_temps[i])
-
-            if i == 0:
-                index = np.where(gas_data['temperature'] <= new_temps[i])
-            elif i == (len(new_temps) - 1):
-                index = np.where(gas_data['temperature'] > new_temps[i])
+        for Z in elements:                
+            if Z == 0:
+                # This is the electron-electron bremstrahlung component alone
+                # Set all abundances to 1 (We need a full census of electrons in the plasma for e-e brems)
+                cie.set_abund(elements[start_idx:], 1.0)
+                # Turn on e-e bremsstrahlung
+                cie.set_eebrems(True)
+                spectrum = cie.return_spectrum(kT, dolines=False, docont=False, dopseudo=False)
             else:
-                index = np.where((gas_data['temperature'] >= new_temps[i - 1]) & (gas_data['temperature'] < new_temps[i]))
+                # This is everything else (remember it is element by element)
+                # First, turn off all the elements.
+                cie.set_abund(elements[start_idx:], 0.0)
+                
+                # Turn back on only THIS element (Z)
+                cie.set_abund(Z, 1.0)
+                
+                # Turn off e-e bremsstrahlung (avoid double counting)
+                cie.set_eebrems(False)
+                spectrum = cie.return_spectrum(kT)
 
-            for j in range(num_atomic):
-                print('Emission j=%d, eps=%g' % (j, float(new_cont[j][i] + new_line[j][i])))
-                eps_total[j][index] += new_cont[j][i] + new_line[j][i]
+            # convert to keV cm3 s-1, sum
+            res['power'][i][Z] = np.sum(spectrum * average_energies)
 
-        lambda_total = eps_total[0] + eps_total[1]
-        lambda_total += (gas_data['carbon'] / solar['carbon']) * eps_total[2]
-        lambda_total += (gas_data['oxygen'] / solar['oxygen']) * eps_total[3]
-        lambda_total += (gas_data['silicon'] / solar['silicon']) * eps_total[4]
-        lambda_total += (gas_data['iron'] / solar['iron']) * eps_total[5]
+    return res
 
-        return lambda_total
+# D. Rennehan: Modified from https://atomdb.readthedocs.io/en/master/examples.html#make-cooling-curve
+def get_power_grid(elements, energy_min, energy_max):
+    if energy_min < 0.001:
+        print('Energy below minimum 0.001keV.')
+        raise ValueError
+    if energy_max > 100.0:
+        print('Energy above maximum 100keV.')
+        raise ValueError
 
-    lambda_factor = gas_data['electron_number_density'] * gas_data['hydrogen_number_density']
-    lambda_factor *= gas_data['volume']
+    # Temperatures at which to calculate curve (K)
+    temperatures = np.logspace(4, 9, 51)
 
-    luminosities = []
-    for i in range(len(bands)):
-        luminosities.append(calculate_lambda(emissivity_files[i]) * lambda_factor)
+    # Set up the spectrum
+    cie = pyatomdb.spectrum.CIESession()
+    energy_bins = np.linspace(energy_min, energy_max, 10001)
+    cie.set_response(energy_bins, raw=True)
+    cie.set_eebrems(True)
+
+
+    result = calculate_power(cie, elements, temperatures)
+
+    # Save a 2D grid of Element, Temperature so that we can interpolate
+    power_grid = np.zeros((len(elements), len(temperatures)))
+    for i, Z in enumerate(elements):
+        for j, T in enumerate(temperatures):
+            # result['power'] is indexed by the actual element Z.
+            # We reverse the indexing here, so that it is Element,Temperature indexed
+            # by the ACTUAL index rather than the atomic number or Tindex.
+            power_grid[i, j] = result['power'][j][Z] if result['power'][j][Z] > 0 else 1.0e-99
+
+    # Interpolate over all temperatures. Final result will be in erg * cm**3 * s**-1.
+    # We go back to the convention of having atomic number as the accessor in the
+    # result dictionary.
+    #
+    # result['interpolated_power'][Z] contains an interp1d object that can be
+    # used to interpolate between the temperatures. That should allow you to
+    # compute the X-ray luminosity of a gas particle with any temperature.
+    result.update({'interpolated_power': {}})
+    for i, Z in enumerate(elements):
+        result['interpolated_power'].update({Z: None})
+        result['interpolated_power'][Z] = interpolate.interp1d(np.log10(result['temperature']), 
+                                                               np.log10(power_grid[i, :] * pyatomdb.const.ERG_KEV), 
+                                                               kind='linear')
+        
+    return result['interpolated_power']
 
